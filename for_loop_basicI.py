4cbf7a9ba2e60dba7471534e375a3f212dffd770
jerome4914 <jeremyballew@hotmail.com>
2021-06-10 01:40:28 -0600

    initial commit

---
+++ for_loop_basicI.py
@@ -0,0 +1,35 @@
+#1
+# for x in range(0, 151, 1):
+#     print(x)
+
+#2
+# for x in range(0, 1001, 5):
+#     print(x)
+
+#3
+# for x in range(0, 101, 1):
+#     if (x) % 5 == 0:
+#         print("Coding")
+#     else:
+#         print(x)
+#     if (x) % 10 == 0:
+#         print("Coding Dojo")
+
+#4
+# sum=0
+# for x in range(1, 5000, 2):
+#     if (x) % 2 == 1:
+#         sum = sum + x
+# print(sum)
+
+#5
+# for x in range(2018, 0, -4):
+#     print(x)
+
+#6
+lownum = int(10)
+highnum = int(100)
+mult = int(10)
+for x in range(lownum, highnum + 1,):
+    if (x % mult == 0):
+        print(x)
